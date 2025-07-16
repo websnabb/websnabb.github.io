84b03a1e395d7d86134b98ed9a7e0c7cc7511a3e
WebSnabb <websnabb@gmail.com>
2025-07-16 14:05:27 +0000

    Ran NPM install and changed the base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import compress from 'astro-compress';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://websnabb.github.io',
-  base: 'my-repo',
+  base: '/',
   experimental: {
     viewTransitions: true,
   },
